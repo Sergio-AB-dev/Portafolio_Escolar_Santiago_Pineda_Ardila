0c2a023e3e30e3751b212cc8ef2b6e2c45dd8bdc
Sergio Ardila <sardila232@gmail.com>
2026-08-09 19:26:10 -0500

    Corregir rutas de Vite

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Portafolio_Escolar_Santiago_Pineda_Ardila', // <-- Pon el nombre de tu repositorio aquí
+  base: '/Portafolio_Escolar_Santiago_Pineda_Ardila/', 
 })
